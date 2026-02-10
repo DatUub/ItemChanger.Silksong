4addff245b4809b4df324b964bb9fabe76ac9373
Uub <aricartagena@icloud.com>
2026-02-09 20:49:35 -0500

    clean up QuillItem, use PlayerData.instance

--- ItemChanger.Silksong/Items/QuillItem.cs
+++ ItemChanger.Silksong/Items/QuillItem.cs
@@ -8,24 +8,16 @@ namespace ItemChanger.Silksong.Items
 
         public override void GiveImmediate(GiveInfo info)
         {
-            var pd = GameManager.instance?.playerData;
-            if (pd == null) return;
-            
-            pd.SetInt("QuillState", QuillState);
-            pd.SetBool("hasQuill", true);
+            PlayerData.instance.SetInt("QuillState", QuillState);
+            PlayerData.instance.SetBool("hasQuill", true);
             
             var quillItem = CollectableItemManager.GetItemByName("Quill");
             quillItem?.Collect(1, showPopup: false);
-            
-            string colorName = QuillState == 1 ? "White" : QuillState == 2 ? "Red" : "Purple";
-            ItemChangerPlugin.Instance.Logger.LogInfo($"Obtained {colorName} Quill (State {QuillState})");
         }
 
         public override bool Redundant()
         {
-            var pd = GameManager.instance?.playerData;
-            if (pd == null) return false;
-            return pd.GetInt("QuillState") == QuillState;
+            return PlayerData.instance.GetInt("QuillState") == QuillState;
         }
     }
 }
