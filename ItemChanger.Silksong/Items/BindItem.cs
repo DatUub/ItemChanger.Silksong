aadc55dbb9c778f8ce3781847b024554a55d8396
Uub <aricartagena@icloud.com>
2026-02-14 16:32:04 -0500

    Refactor BindItem to use PlayerData for binding

--- ItemChanger.Silksong/Items/BindItem.cs
+++ ItemChanger.Silksong/Items/BindItem.cs
@@ -1,24 +1,20 @@
+using ItemChanger; 
 using ItemChanger.Items;
-using ItemChanger.Silksong.Modules;
 
 namespace ItemChanger.Silksong.Items
 {
     public sealed class BindItem : Item
     {
+        public const string BindSkillKey = "hasBindSkill";
+
         public override void GiveImmediate(GiveInfo info)
         {
-            var module = ActiveProfile?.Modules.Get<BindSkill>();
-            if (module == null)
-            {
-                module = new BindSkill();
-                ActiveProfile?.Modules.Add(module);
-            }
-            module.CanBind = true;
+            PlayerData.instance.SetBool(BindSkillKey, true);
         }
 
         public override bool Redundant()
         {
-            return BindSkill.Instance?.CanBind ?? false;
+            return PlayerData.instance.GetBool(BindSkillKey);
         }
     }
 }
