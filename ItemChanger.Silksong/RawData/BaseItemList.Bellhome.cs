fa835f8ca7fd1f64c16f13a8fe37b0f40c0c2473
homothetyhk <homothetyhk@gmail.com>
2026-03-07 10:08:50 -0800

    Reorgnanize bellhome items, remove duplicate item class.

--- ItemChanger.Silksong/RawData/BaseItemList.Bellhome.cs
+++ ItemChanger.Silksong/RawData/BaseItemList.Bellhome.cs
@@ -7,21 +7,6 @@ namespace ItemChanger.Silksong.RawData;
 internal static partial class BaseItemList
 {
     //bellhome upgrades
-    //TODO: implement ItemChanger support for non-CollectableItem bellhome upgrades
-    //note: i think the bellhome lacquers are one time use and can not overlap
-    //also the bought bellhome upgrades seem to be based on PlayerData bool values
-    /* unimplemented items
-        Bell_Lacquer__Black -> BelltownHouseColour
-        Bell_Lacquer__Blue -> BelltownHouseColour
-        Bell_Lacquer__Bronze -> BelltownHouseColour
-        Bell_Lacquer__Chrome -> BelltownHouseColour [steel soul exclusive]
-        Bell_Lacquer__Red -> BelltownHouseColour
-        Bell_Lacquer__White -> BelltownHouseColour [replaced by Bell_Lacquer__Chrome in steel soul]
-        Desk -> BelltownFurnishingDesk
-        Gleamlights -> BelltownFurnishingFairyLights
-        Gramophone -> BelltownFurnishingGramaphone
-        Personal_Spa -> BelltownFurnishingSpa
-    */
     public static Item Crawbell => ItemChangerSavedItem.Create(
         name: ItemNames.Crawbell,
         id: "Crawbell",
@@ -34,4 +19,16 @@ internal static partial class BaseItemList
         name: ItemNames.Materium,
         id: "Materium",
         type: BaseGameSavedItem.ItemType.CollectableItem);
+
+    public static Item Desk => new PDBoolItem { Name = ItemNames.Desk, BoolName = nameof(PlayerData.BelltownFurnishingDesk), UIDef = null! };
+    public static Item Gleamlights => new PDBoolItem { Name = ItemNames.Gleamlights, BoolName = nameof(PlayerData.BelltownFurnishingFairyLights), UIDef = null! };
+    public static Item Gramophone => new PDBoolItem { Name = ItemNames.Gramophone, BoolName = nameof(PlayerData.BelltownFurnishingGramaphone), UIDef = null! };
+    public static Item Personal_Spa => new PDBoolItem { Name = ItemNames.Personal_Spa, BoolName = nameof(PlayerData.BelltownFurnishingSpa), UIDef = null! };
+
+    public static Item Bell_Lacquer__Red => new PDIntItem { Name = ItemNames.Bell_Lacquer__Red, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 1, Increment = false, UIDef = null! };
+    public static Item Bell_Lacquer__White => new PDIntItem { Name = ItemNames.Bell_Lacquer__White, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 2, Increment = false, UIDef = null! };
+    public static Item Bell_Lacquer__Black => new PDIntItem { Name = ItemNames.Bell_Lacquer__Black, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 3, Increment = false, UIDef = null! };
+    public static Item Bell_Lacquer__Bronze => new PDIntItem { Name = ItemNames.Bell_Lacquer__Bronze, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 4, Increment = false, UIDef = null! };
+    public static Item Bell_Lacquer__Blue => new PDIntItem { Name = ItemNames.Bell_Lacquer__Blue, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 5, Increment = false, UIDef = null! };
+    public static Item Bell_Lacquer__Chrome => new PDIntItem { Name = ItemNames.Bell_Lacquer__Chrome, IntName = nameof(PlayerData.BelltownHouseColour), Amount = 6, Increment = false, UIDef = null! };
 }
