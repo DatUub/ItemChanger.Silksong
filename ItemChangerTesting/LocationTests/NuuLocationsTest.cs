d9b7754a6f9b45668069d8eb332bf575a8f100f3
homothetyhk <homothetyhk@gmail.com>
2026-07-25 18:54:33 -0400

    Test all four Nuu locations, add convenience extensions.

--- ItemChangerTesting/LocationTests/NuuLocationsTest.cs
+++ ItemChangerTesting/LocationTests/NuuLocationsTest.cs
@@ -1,9 +1,7 @@
 using Benchwarp.Data;
 using ItemChanger;
-using ItemChanger.Enums;
 using ItemChanger.Silksong.Modules.BossKillsCounter;
 using ItemChanger.Silksong.RawData;
-using ItemChanger.Tags;
 using PrepatcherPlugin;
 
 namespace ItemChangerTesting.LocationTests;
@@ -14,7 +12,7 @@ internal class NuuLocationsTest : Test
     {
         Folder = TestFolder.LocationTests,
         MenuName = "Nuu",
-        MenuDescription = "Tests giving items from both Nuu locations",
+        MenuDescription = "Tests giving items from all four Nuu locations",
         Revision = 20260423,
     };
 
@@ -22,24 +20,39 @@ internal class NuuLocationsTest : Test
     {
         StartNear(SceneNames.Halfway_01, PrimitiveGateNames.left1);
 
+        Profile.AddPlacement(Finder.GetLocation(LocationNames.Hunter_s_Journal)!.Wrap()
+            .WithDebugItem()
+            .WithAllPersistent()
+            );
+
+        Profile.AddPlacement(Finder.GetLocation(LocationNames.Lore_Tablet__Nuu_Scroll)!.Wrap()
+            .WithDebugItem()
+            .WithAllPersistent()
+            );
+
         Profile.AddPlacement(Finder.GetLocation(LocationNames.Tool_Pouch__Nuu)!.Wrap()
-            .Add(Finder.GetItem(ItemNames.Flea)!));
+            .WithDebugItem()
+            .WithAllPersistent()
+            );
 
         Profile.AddPlacement(Finder.GetLocation(LocationNames.Hunter_s_Memento)!.Wrap()
-            .Add(Finder.GetItem(ItemNames.Surgeon_s_Key)!.WithTag(new PersistentItemTag()
-                { Persistence = Persistence.Persistent })));
+            .WithDebugItem()
+            .WithAllPersistent()
+            );
     }
 
     protected override void OnEnterGame()
     {
         // Need act 3 for memento check
-        PlayerDataAccess.blackThreadWorld = true;
-        PlayerDataAccess.act3_enclaveWakeSceneCompleted = true;
-        PlayerDataAccess.act3_wokeUp = true;
+        //PlayerDataAccess.blackThreadWorld = true;
+        //PlayerDataAccess.act3_enclaveWakeSceneCompleted = true;
+        //PlayerDataAccess.act3_wokeUp = true;
+        //PlayerDataAccess.nuuMementoAwarded = true;
     }
     
     public override IEnumerable<(string, Action)> TestMethods()
     {
+        yield return ("Start Act 3", StartAct3);
         yield return ("Grant 3 Boss Entries", () => GrantBossEntries(3));
         yield return ("Grant 10 Boss Entries", () => GrantBossEntries(10));
     }
@@ -48,15 +61,15 @@ internal class NuuLocationsTest : Test
     {
         var mod = Modules.GetOrAdd<BossKillsCounterModule>();
         
-        foreach (var boss in mod.BossDefinitions)
+        foreach (var boss in mod.BossCounters.Values)
         {
-            if (boss is not JournalEntryBossDefinition entryDefinition)
+            if (boss is not JournalBossCounter counter)
                 continue;
-            var killData = PlayerDataAccess.EnemyJournalKillData.GetKillData(entryDefinition.BossName);
+            var killData = PlayerDataAccess.EnemyJournalKillData.GetKillData(counter.Name);
             if (killData.Kills > 0)
                 continue;
             killData.Kills += 1;
-            PlayerDataAccess.EnemyJournalKillData.RecordKillData(entryDefinition.BossName, killData);
+            PlayerDataAccess.EnemyJournalKillData.RecordKillData(counter.Name, killData);
             killsToAdd--;
             if (killsToAdd == 0)
                 return;
