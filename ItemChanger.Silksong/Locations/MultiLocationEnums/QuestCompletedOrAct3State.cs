73b38d9859bc7f4a4bf2a7616392cfd232ae223b
homothetyhk <homothetyhk@gmail.com>
2026-07-29 11:28:48 -0400

    Add IfThenElse value provider, use to build some enum value providers.

--- ItemChanger.Silksong/Locations/MultiLocationEnums/QuestCompletedOrAct3State.cs
+++ ItemChanger.Silksong/Locations/MultiLocationEnums/QuestCompletedOrAct3State.cs
@@ -1,7 +1,3 @@
-using ItemChanger.Serialization;
-using Newtonsoft.Json;
-using PrepatcherPlugin;
-
 namespace ItemChanger.Silksong.Locations.MultiLocationEnums;
 
 public enum QuestCompletedOrAct3State
@@ -20,30 +16,4 @@ public enum QuestCompletedOrAct3State
     /// The world is in Act 3 state; quest may or may not be completed.
     /// </summary>
     Act3
-}
-
-/// <summary>
-/// A value provider indicating whether the world is in Act 3 and, if not, whether a quest has been completed.
-/// </summary>
-public class QuestCompletedOrAct3StateProvider : IValueProvider<QuestCompletedOrAct3State>
-{
-    /// <summary>
-    /// The quest whose completion state should be inspected.
-    /// </summary>
-    public required string Quest { get; init; }
-
-    [JsonIgnore]
-    public QuestCompletedOrAct3State Value
-    {
-        get
-        {
-            if (PlayerDataAccess.blackThreadWorld)
-                return QuestCompletedOrAct3State.Act3;
-
-            if (QuestManager.GetQuest(Quest).IsCompleted)
-                return QuestCompletedOrAct3State.QuestComplete;
-
-            return QuestCompletedOrAct3State.QuestIncomplete;
-        }
-    }
 }
